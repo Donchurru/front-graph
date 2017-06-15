72e08021d3066b4442ad0d8e788f76949edd0107
Ruben Romero <rubenrmatias@gmail.com>
2017-06-15 10:35:22 -0500

    Restructuracion de archivos y añadido menu lateral al hacer click

--- resources/js/graph-creator.js
+++ resources/js/graph-creator.js
@@ -1,11 +1,12 @@
-document.onload = (function(d3, saveAs, Blob, undefined) {
-  "use strict";
+//document.onload = (function(d3, saveAs, Blob, undefined) {
+//$( document ).ready(function() {
+//  "use strict";
 
   // define graphcreator object
   var GraphCreator = function(svg, nodes, edges) {
     var thisGraph = this;
     thisGraph.idct = 0;
-
+    thisGraph.d3 = d3;
     thisGraph.nodes = nodes || [];
     thisGraph.edges = edges || [];
 
@@ -430,8 +431,10 @@ document.onload = (function(d3, saveAs, Blob, undefined) {
 
           if (!prevNode || prevNode.id !== d.id) {
             thisGraph.replaceSelectNode(d3node, d);
+            Graph.onSelectNode();
           } else {
             thisGraph.removeSelectFromNode();
+            Graph.onDeselectNode();
           }
         }
       }
@@ -491,7 +494,7 @@ document.onload = (function(d3, saveAs, Blob, undefined) {
     var selectedNode = state.selectedNode,
       selectedEdge = state.selectedEdge;
 
-    switch (d3.event.keyCode) {
+    /*switch (d3.event.keyCode) {
       case consts.BACKSPACE_KEY:
       case consts.DELETE_KEY:
         d3.event.preventDefault();
@@ -506,7 +509,7 @@ document.onload = (function(d3, saveAs, Blob, undefined) {
           thisGraph.updateGraph();
         }
         break;
-    }
+    }*/
   };
 
   GraphCreator.prototype.svgKeyUp = function() {
@@ -610,145 +613,5 @@ document.onload = (function(d3, saveAs, Blob, undefined) {
 
   /**** MAIN ****/
 
-  // warn the user when leaving
-  window.onbeforeunload = function() {
-    return "Make sure to save your graph locally before leaving :-)";
-  };
-
-  var docEl = document.documentElement,
-    bodyEl = document.getElementsByTagName('body')[0];
-
-  var width = window.innerWidth || docEl.clientWidth || bodyEl.clientWidth,
-    height = window.innerHeight || docEl.clientHeight || bodyEl.clientHeight;
-
-  var xLoc = width / 2 - 25,
-    yLoc = 100;
-
-  // initial node data
-  var nodes = [{
-    id: 0,
-    type: "table",
-    title: "TCDT050",
-    x: xLoc - 150,
-    y: yLoc - 150,
-    query: {
-      type: "sql",
-      tables: [
-        "TCDT050"
-      ],
-      "fields": [
-        "all"
-      ],
-      filters: [{
-        field: "TCDT050.CAMPO_EJEMPLO",
-        "operator": "<",
-        "value": 10
-      }],
-      matchers: []
-    }
-  }, {
-    id: 1,
-    type: "Actor",
-    title: "Standarizer",
-    x: xLoc - 100,
-    y: yLoc - 100,
-
-    actions: [{
-      "sourceField": "COD_ENT_ORIG",
-      "targetField": "COD_ENT",
-      "action": "left-pad",
-      "mask": "0000"
-    }, {
-      "sourceField": "COD_CENTRO_ORIG",
-      "targetField": "COD_CENTRO",
-      "action": "left-pad",
-      "mask": "0000"
-    }]
-  }, {
-    id: 2,
-    type: "Actor",
-    title: "Mandatory",
-    x: xLoc + 300,
-    y: yLoc + 150,
-
-    actions: [{
-      field: "COD_ENT_ORIG"
-    }, {
-      field: "COD_CENTRO_ORIG"
-    }]
-  }, {
-    id: 3,
-    type: "sql",
-    title: "DomainValidation",
-    x: xLoc + 200,
-    y: yLoc + 250,
-
-    query: {
-      type: "sql",
-      tables: [
-        "TCDT040"
-      ],
-      "fields": [
-        "all"
-      ],
-      filters: [{
-        field: "TCDT040.CATALOG_VERSION",
-        "operator": "=",
-        "value": 2
-      }],
-      matchers: [
-        "TCDT050.COD_ENT",
-        "TCDT040.COD_ENT"
-      ]
-    }
-  }, {
-    id: 4,
-    type: "table",
-    title: "TCDT050 (accepted)",
-    x: xLoc + 350,
-    y: yLoc - 350,
-
-  }, {
-    id: 5,
-    type: "table",
-    title: "TCDT050 (rejected)",
-    x: xLoc - 450,
-    y: yLoc + 350,
-
-  }];
-  var edges = [{
-    source: nodes[0],
-    target: nodes[1],
-    title: "rows"
-  }, {
-    source: nodes[1],
-    target: nodes[2],
-    title: "acceptedAndRejectedRows"
-  }, {
-    source: nodes[2],
-    target: nodes[3],
-    title: "acceptedRows"
-  }, {
-    source: nodes[2],
-    target: nodes[5],
-    title: "rejectedRows"
-  }, {
-    source: nodes[3],
-    target: nodes[4],
-    title: "acceptedRows"
-  }, {
-    source: nodes[3],
-    target: nodes[5],
-    title: "rejectedRows"
-  }];
-
-
-  /** MAIN SVG **/
-  var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-  var graph = new GraphCreator(svg, nodes, edges);
-  graph.setIdCt(nodes.length + 1);
-  graph.setIdCt(6);
-  graph.updateGraph();
-})(window.d3, window.saveAs, window.Blob);
+  
+//})(window.d3, window.saveAs, window.Blob);
---
+++ resources/js/main.js
@@ -0,0 +1,180 @@
+
+$( document ).ready(function() {
+	Graph.initGraph();
+});
+
+var Graph = {
+	graph : null,
+	initGraph : function(){
+		// warn the user when leaving
+	  window.onbeforeunload = function() {
+	    return "Make sure to save your graph locally before leaving :-)";
+	  };
+
+	  $('#mySidenav .closebtn').click(function(){
+	  	Graph.graph.removeSelectFromNode();
+	  	Graph.closeNav();
+	  });
+
+	  var docEl = document.documentElement,
+	    bodyEl = document.getElementsByTagName('body')[0];
+
+	  var width = window.innerWidth || docEl.clientWidth || bodyEl.clientWidth,
+	    height = window.innerHeight || docEl.clientHeight || bodyEl.clientHeight;
+
+	  var xLoc = width / 2 - 25,
+	    yLoc = 100;
+
+	  // initial node data
+	  var nodes = [{
+	    id: 0,
+	    type: "table",
+	    title: "TCDT050",
+	    x: xLoc - 150,
+	    y: yLoc - 150,
+	    query: {
+	      type: "sql",
+	      tables: [
+	        "TCDT050"
+	      ],
+	      "fields": [
+	        "all"
+	      ],
+	      filters: [{
+	        field: "TCDT050.CAMPO_EJEMPLO",
+	        "operator": "<",
+	        "value": 10
+	      }],
+	      matchers: []
+	    }
+	  }, {
+	    id: 1,
+	    type: "Actor",
+	    title: "Standarizer",
+	    x: xLoc - 100,
+	    y: yLoc - 100,
+
+	    actions: [{
+	      "sourceField": "COD_ENT_ORIG",
+	      "targetField": "COD_ENT",
+	      "action": "left-pad",
+	      "mask": "0000"
+	    }, {
+	      "sourceField": "COD_CENTRO_ORIG",
+	      "targetField": "COD_CENTRO",
+	      "action": "left-pad",
+	      "mask": "0000"
+	    }]
+	  }, {
+	    id: 2,
+	    type: "Actor",
+	    title: "Mandatory",
+	    x: xLoc + 300,
+	    y: yLoc + 150,
+
+	    actions: [{
+	      field: "COD_ENT_ORIG"
+	    }, {
+	      field: "COD_CENTRO_ORIG"
+	    }]
+	  }, {
+	    id: 3,
+	    type: "sql",
+	    title: "DomainValidation",
+	    x: xLoc + 200,
+	    y: yLoc + 250,
+
+	    query: {
+	      type: "sql",
+	      tables: [
+	        "TCDT040"
+	      ],
+	      "fields": [
+	        "all"
+	      ],
+	      filters: [{
+	        field: "TCDT040.CATALOG_VERSION",
+	        "operator": "=",
+	        "value": 2
+	      }],
+	      matchers: [
+	        "TCDT050.COD_ENT",
+	        "TCDT040.COD_ENT"
+	      ]
+	    }
+	  }, {
+	    id: 4,
+	    type: "table",
+	    title: "TCDT050 (accepted)",
+	    x: xLoc + 350,
+	    y: yLoc - 350,
+
+	  }, {
+	    id: 5,
+	    type: "table",
+	    title: "TCDT050 (rejected)",
+	    x: xLoc - 450,
+	    y: yLoc + 350,
+
+	  }];
+	  var edges = [{
+	    source: nodes[0],
+	    target: nodes[1],
+	    title: "rows"
+	  }, {
+	    source: nodes[1],
+	    target: nodes[2],
+	    title: "acceptedAndRejectedRows"
+	  }, {
+	    source: nodes[2],
+	    target: nodes[3],
+	    title: "acceptedRows"
+	  }, {
+	    source: nodes[2],
+	    target: nodes[5],
+	    title: "rejectedRows"
+	  }, {
+	    source: nodes[3],
+	    target: nodes[4],
+	    title: "acceptedRows"
+	  }, {
+	    source: nodes[3],
+	    target: nodes[5],
+	    title: "rejectedRows"
+	  }];
+
+
+	  /** MAIN SVG **/
+	  var svg = d3.select("body").append("svg")
+	    .attr("width", width)
+	    .attr("height", height);
+	  var graph = new GraphCreator(svg, nodes, edges);
+	  graph.setIdCt(nodes.length + 1);
+	  graph.setIdCt(6);
+	  graph.updateGraph();
+	  Graph.graph = graph;
+	},
+	onSelectNode : function () {
+		Graph.openNav();
+		var selectedNode = Graph.graph.state.selectedNode;
+		$("#node-name").val(selectedNode.title);
+	},
+
+	onDeselectNode : function () {
+		Graph.closeNav();
+	},
+
+	openNav : function(){
+		$("#mySidenav").animate({width: 'show'}, "slow");
+		//var transform = Graph.graph.d3.transform($(Graph.graph.svgG[0][0]).attr("transform"));
+		//transform.translate[0] = transform.translate[0] + 300;
+		//$(Graph.graph.svgG[0][0]).attr("transform",transform.toString());
+	},
+
+	closeNav : function(){
+		$("#mySidenav").animate({width: 'hide'}, "slow");
+		//var transform = Graph.graph.d3.transform($(Graph.graph.svgG[0][0]).attr("transform"));
+		//transform.translate[0] = transform.translate[0] - 300;
+		//$(Graph.graph.svgG[0][0]).attr("transform",transform.toString());
+	}
+}
